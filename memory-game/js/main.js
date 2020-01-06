ffdfb9e101110688ad44f244cd01bbf8f0abf029
ttolentino89 <trishatolentino89@gmail.com>
2020-01-05 23:26:59 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,4 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
 let cardOne = "queen";
 let cardTwo = "king";
@@ -7,3 +7,20 @@ console.log("User flipped " + cardOne);
 
 // let cardThree;
 // let cardFour;
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+// let cardOne;
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+	}
+}
